e30771dc2545867cb24e4379adc345c54f1cc11b
Pikka2048 <0312pika0201@gmail.com>
2022-08-06 14:27:49 +0900

    Edit Grass Color

--- src/GrassTile.js
+++ src/GrassTile.js
@@ -6,16 +6,16 @@ import Tooltip from './Tooltip';
 function GrassColorByColor(m_power) {
     let power = m_power;
     if (power <= 0) {
-        return "rgb(20,40,20)";
+        return "rgb(220,220,220)";
     }
     else if (1 <= power && power < 3) {
-        return "rgb(20, 100, 20)";
+        return "rgb(155, 233, 168)";
     }
     else if (3 <= power && power < 10) {
-        return "rgb(20,170,20)";
+        return "rgb(65, 195, 100)";
     }
     else if (10 <= power) {
-        return "rgb(20,240,20)";
+        return "rgb(32, 111, 56)";
     }
     console.log(power);
     return "err";
