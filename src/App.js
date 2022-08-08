f9413eddfc90fc2c52479074a43c43f924dee254
Pikka2048 <0312pika0201@gmail.com>
2022-08-08 17:12:09 +0900

    Update add your any date

--- src/App.js
+++ src/App.js
@@ -4,36 +4,48 @@ import './App.css';
 import TodoItem from './TodoItem';
 import Grass from './Grass';
 
-function getRandomArbitrary(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.random() * (max - min) + min;
-}
-
 //Now
 const dtStart = new Date();
 let GrassDate = [];
 for (let i = 0; i < 364; i++) {
   dtStart.setDate(dtStart.getDate() + 1);
   let set = String(dtStart.getFullYear()) + "/" + String(dtStart.getMonth() + 1) + "/" + String(dtStart.getDate());
-  GrassDate.push([set, getRandomArbitrary(-10, 14)]);
+  GrassDate.push([set, 0]);
 }
-console.log(GrassDate)
-
-const GrassDate2 = [
-  ["10/21", 0],
-  ["10/22", 1],
-  ["10/23", 2],
-  ["10/24", 3],
-  ["10/25", 4],
-  ["10/26", 5],
-  ["10/27", 6],
-  ["10/28", 7],
-  ["10/29", 8],
-  ["10/30", 9],
-  ["10/31", 10],
-  ["11/01", 11],
+let GrassDate2 = [
+  ["2022/10/21", 0],
+  ["2022/10/22", 1],
+  ["2022/10/23", 2],
+  ["2022/10/24", 3],
+  ["2022/10/25", 4],
+  ["2022/10/26", 5],
+  ["2022/10/27", 6],
+  ["2022/10/28", 7],
+  ["2022/10/29", 8],
+  ["2022/10/30", 9],
+  ["2022/10/31", 10],
+  ["2022/11/01", 11],
 ]
+// If the dates match, we overwrite them in the second array.
+// let a = [["hello",1],["good",3],["bye",4]]
+// let b = [["hi",1],   ["good",2],["bye",5]]
+// result :[["hello",1],["good",2],["bye",5]]
+
+function MergeArrayOverWriteSecound(base, added) {
+  let len_base = Object.keys(base).length;
+  let len_added = Object.keys(added).length;
+
+  for (let b = 0; b < len_base; b++) {
+    for (let a = 0; a < len_added; a++) {
+      if (base[b][0] == added[a][0]) {
+        base[b] = added[a];
+      }
+    }
+  }
+  return base;
+}
+
+let mergetest = MergeArrayOverWriteSecound(GrassDate, GrassDate2);
 
 function App() {
   const [TaskName, setTaskName] = useState('')
@@ -87,7 +99,7 @@ function App() {
         </form>
       </div>
       <div className='Grass'>
-        <Grass data={GrassDate}></Grass>
+        <Grass data={mergetest}></Grass>
       </div>
     </div>
 
